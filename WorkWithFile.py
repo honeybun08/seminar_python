5c03a207e803bc4727d0517ba1d19f2e7ac632a6
Ulyana <ulyana_yezhgurova@mail.ru>
2023-08-23 12:48:29 +0300

    work with file of sem8

---
+++ WorkWithFile.py
@@ -0,0 +1,20 @@
+def add_person(person):
+    data = open('Dict.txt', 'a', encoding='utf-8')
+    data.write(person)
+    data.write('\n')
+    data.close()
+
+def show_all():
+    data = open ('Dict.txt', 'r', encoding = 'utf-8')
+    for line in data:
+        print(line[:-1])
+    data.close()
+
+def search_element(name):
+    with open ('Dict.txt', 'r', encoding = 'utf-8') as data:
+        for line in data:
+            if name in line.split():
+                print(line)
+                break
+        else:
+            print('Такого нету')
