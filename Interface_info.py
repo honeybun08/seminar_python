d43515ba70674a57bed696e00f5eec3204ef4333
Ulyana <ulyana_yezhgurova@mail.ru>
2023-08-25 22:45:11 +0300

    2nd variant of hw

--- Interface_info.py
+++ Interface_info.py
@@ -1,13 +1,13 @@
-import os
-from hw8_task1 import *
-clear = lambda: os.system('cls' if os.name=='nt' else 'clear')
+# import os
+# from hw8_task1 import *
+# clear = lambda: os.system('cls' if os.name=='nt' else 'clear')
 
 
-while True:
-    clear()
-    print('Выберите операцию: \n 1 - Добавление контакта \n 2 - Вывод по фамилии \n 3 - Удаление данных '
-           '\n 4 - Изменение данных \n 5 - Вывод всех контактов \n 6 - Выход')
-    choose()
-    input('Введите ENTER чтобы продолжить')
+# while True:
+#     clear()
+#     print('Выберите операцию: \n 1 - Добавление контакта \n 2 - Вывод по фамилии \n 3 - Удаление данных '
+#            '\n 4 - Изменение данных \n 5 - Вывод всех контактов \n 6 - Выход')
+#     choose()
+#     input('Введите ENTER чтобы продолжить')
 
   
